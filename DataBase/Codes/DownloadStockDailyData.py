c3d795f1e9d2830a28a1e855c80758f96915cbde
盛亦青(Sheng, Yiqing) <40531026+ShengYiqing@users.noreply.github.com>
2020-11-15 03:20:33 +0800

    Add files via upload

--- DataBase/Codes/DownloadStockDailyData.py
+++ DataBase/Codes/DownloadStockDailyData.py
@@ -12,7 +12,7 @@ today = datetime.datetime.today().strftime('%Y%m%d')
 if len(sys.argv) > 1:
     start = sys.argv[1]
 else:
-    start = '20050101'
+    start = '20150101'
 end = today
 
 pro = ts.pro_api()
@@ -24,7 +24,6 @@ for stock in stocks:
     df = pd.concat([
         pro.daily(ts_code=stock, start_date=start, end_date=end, fields='ts_code, trade_date, open, high, low, close, vol, amount').set_index('trade_date'),
         pro.adj_factor(ts_code=stock, start_date=start, end_date=end, fields='trade_date, adj_factor').set_index('trade_date'),
-        pro.daily_basic(ts_code=stock, start_date=start, end_date=end, fields='trade_date, turnover_rate_f, pe_ttm, pb, ps_ttm, dv_ttm, total_mv, circ_mv').set_index('trade_date'),
     ], axis=1, sort=False)
     st = DataFrame(False, index=df.index, columns=[stock])
     nc = pro.namechange(ts_code=stock)
@@ -37,15 +36,12 @@ for stock in stocks:
     st.columns = ['st']
     df = pd.concat([df, st], axis=1, sort=False)
     df = df.sort_index()
-    '''
-    df = pro.daily(ts_code=stock, start_date=start, end_date=end).set_index('trade_date')
-    df.loc[:, 'adj_factor'] = pro.adj_factor(ts_code=stock, start_date=start, end_date=end).set_index('trade_date').adj_factor
-    df = pd.concat([df, pro.daily_basic(ts_code=stock, start_date=start, end_date=end, fields='trade_date, turnover_rate, turnover_rate_f, volume_ratio, pe_ttm, pb, ps_ttm, total_mv, circ_mv').set_index('trade_date')], axis=1, sort=False)
-    df = pd.concat([df, pro.moneyflow(ts_code=stock, start_date=start, end_date=end, fields='trade_date, buy_sm_vol, sell_sm_vol, buy_md_vol, sell_md_vol, buy_lg_vol, sell_lg_vol, buy_elg_vol, sell_elg_vol, net_mf_vol').set_index('trade_date')], axis=1, sort=False)
-    df = df.sort_index()
-    '''
-    df.to_csv('../StockDailyData/Stock/%s.csv'%stock)
+    
     if len(df) > 0:
+        df.to_csv('../StockDailyData/Stock/%s.csv'%stock)
         if df.notna().iloc[-1, :].any():
             df_daily = pd.concat([df_daily, df.iloc[-1:, :]], axis=0, sort=False)
-df_daily.set_index('ts_code').to_csv('../StockDailyData/Daily/%s.csv'%today)
+df_daily.set_index('ts_code').to_csv('../StockDailyData/Daily/%s.csv'%df.index[-1])
+
+
+#pro.daily_basic(ts_code=stock, start_date=start, end_date=end, fields='trade_date, turnover_rate_f, pe_ttm, pb, ps_ttm, dv_ttm, total_mv, circ_mv').set_index('trade_date'),
